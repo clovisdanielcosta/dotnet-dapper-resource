fb4816047d6fbc5f0aae77208ec7836e7403e9f7
Clóvis Daniel Costa <clovisdaniel@hotmail.com>
2023-12-14 15:50:45 -0300

    Add project files.

---
+++ Repositories/DapperSampleRepository.cs
@@ -0,0 +1,24 @@
+﻿using Dapper;
+using DapperRepositoryResource.Models;
+using DapperRepositoryResource.Repositories.Resources;
+
+namespace DapperRepositoryResource.Repositories
+{
+    public class DapperSampleRepository : RepositoryBase
+    {
+        public async Task<SampleEntity> ObterExemploPorId(string id)
+        {
+            try
+            {
+                using var dbConnection = CriarConexaoAberta();
+
+                return (await dbConnection.QueryAsync<SampleEntity>(SampleEntityResource.SampleEntityConsultaPorId,
+                                        new { ID = id }, commandTimeout: 15)).FirstOrDefault()!;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Repositories/RepositoryBase.cs
@@ -0,0 +1,20 @@
+﻿using MySqlConnector;
+
+namespace DapperRepositoryResource.Repositories
+{
+    public class RepositoryBase
+    {
+        public MySqlConnection CriarConexaoAberta()
+        {
+            var host = "";
+            var port = "";
+            var database = "";
+            var username = "";
+            var password = "";
+
+            var connectionString = $"server='{host}';port='{port}';database='{database}';uid='{username}';password='{password}'";
+
+            return new MySqlConnection(connectionString);
+        }
+    }
+}
